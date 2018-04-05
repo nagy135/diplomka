b8396aa4a6c6eb0e1f549912eccd913df82d61b6
nagy135 <viktor.nagy1995@gmail.com>
2018-04-05 19:01:49 +0200

    recurse point find PSF

--- point_spread_function.py
+++ point_spread_function.py
@@ -19,40 +19,52 @@ class ContinueWhileLoop(Exception):
 continue_while_loop = ContinueWhileLoop()
 
 def create_psf_objects(image):
-    threshold = np.mean(np.array([np.max(image), np.median(image)])) * 1/3
+    threshold = np.mean(np.array([np.max(image), np.median(image)])) * 1/2
 
     mask = image > threshold
     show_image([image,mask], ['image','maska'])
     point_spread_mashes = list()
-    while mask.sum() > 0:
+
+    mask_sum = mask.sum()
+    while mask_sum > 0:
         try:
-            print(mask.sum())
+            print('Points remaining in mask : {}'.format(str(mask_sum)))
             for y, row in enumerate(mask):
                 for x, point in enumerate(row):
                     if point == 1:
-                        points_around = recurrent_point_search(x, y, list(), mask)
+                        points_around = recurrent_point_search(x, y, [], mask)
+                        mask_sum -= len(points_around)
                         point_spread_mashes.append(PointSpreadMesh(points_around))
                         raise continue_while_loop
         except ContinueWhileLoop:
             continue
+    print('Algorithm detected {} point meshes'.format(str(len(point_spread_mashes))))
 
 def recurrent_point_search(x, y, points, mask):
     if mask[y][x] == 1 and (x,y) not in points:
         points.append((x,y))
-    new_points = list()
+        mask[y][x] = 0
+    else:
+        return []
+    new_points = []
     if x-1 >= 0:
-        new_points.append(recurrent_point_search(x-1, y, points, mask))
+        new_points += recurrent_point_search(x-1, y, points, mask)
     if x+1 < mask.shape[1]:
-        new_points.append(recurrent_point_search(x+1, y, points, mask))
+        new_points += recurrent_point_search(x+1, y, points, mask)
     if y-1 >= 0:
-        new_points.append(recurrent_point_search(x, y-1, points, mask))
-    if x+1 < mask.shape[0]:
-        new_points.append(recurrent_point_search(x, y+1, points, mask))
+        new_points += recurrent_point_search(x, y-1, points, mask)
+    if y+1 < mask.shape[0]:
+        new_points += recurrent_point_search(x, y+1, points, mask)
 
-
-    return points + new_points
+    return list(set(points + new_points))
 
 
 image = read_fits_file('M27_R_60s-001.fit')
-# create_psf_objects(image)
-show_3d_data(image)
+# image = np.array([
+#     [0,0,0,0],
+#     [0,1000,1000,0],
+#     [0,1000,1000,0],
+#     [0,0,0,0],
+#     ])
+create_psf_objects(image)
+# show_3d_data(image)
