f6ead31c7c3dbf0b0e25ecc813723555d1a54eb3
nagy135 <viktor.nagy1995@gmail.com>
2018-04-11 14:22:51 +0200

    Point Spread Mesh

--- PointSpreadMesh.py
+++ PointSpreadMesh.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.optimize import curve_fit
+from scipy import asarray as ar,exp
 
 
 class PointSpreadMesh(object):
@@ -8,6 +10,8 @@ class PointSpreadMesh(object):
         self.biggest_intensity_point = None
         self.horizontal_cross = list()
         self.vertical_cross = list()
+        self.popt = None
+        self.pcov = None
 
     def get_point_crosses(self):
         raise NonImplementedError
@@ -16,4 +20,15 @@ class PointSpreadMesh(object):
         return a*exp(-(x-x0)**2/(2*sigma**2))
 
     def fit_curve():
-        raise NonImplementedError
+        self.get_point_crosses()
+        self.horizontal_popt, self.horizontal_pcov = self._inner_fit_curve(np.arange(len(self.horizontal_cross)), self.horizontal_cross)
+        self.vertical_popt, self.vertical_pcov = self._inner_fit_curve(np.arange(len(self.vertical_cross)), self.vertical_cross)
+
+
+    def _inner_fit_curve(x, y):
+        n = len(x)
+        mean = sum(x*y)/n
+        sigma = sum(y*(x-mean)**2)/n
+
+        popt, pcov = curve_fit(self.gaus,x,y,p0=[1,mean,sigma])
+        return popt, pcov
